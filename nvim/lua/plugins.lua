0e65e021391838873b1aa35438cee254c218ec40
aldimhr <dirikuin16@gmail.com>
2025-05-04 18:20:59 +0700

    add config Comment.nvim to ignore empty lines

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -68,6 +68,7 @@ return {
         },
         config = function()
             require('Comment').setup {
+                ignore = '^$',
                 pre_hook = require('ts_context_commentstring.integrations.comment_nvim').create_pre_hook(),
             }
         end,
